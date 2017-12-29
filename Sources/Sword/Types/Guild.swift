d430f30f90766ff573ac69131b9f3a22687518ec
Azoy <aalonso128@outlook.com>
2017-12-29 01:06:17 -0600

    Add botMember to Guild

--- Sources/Sword/Types/Guild.swift
+++ Sources/Sword/Types/Guild.swift
@@ -22,6 +22,15 @@ public class Guild: Updatable, Imageable {
   /// AFK timeout in seconds (if there is any)
   public internal(set) var afkTimeout: Int?
 
+	/// The member type for the bot user
+	public var botMember: Member? {
+		guard let user = self.sword?.user else {
+			return nil
+		}		
+
+		return self.members[user.id]
+	}
+
   /// Collection of channels mapped by channel ID
   public internal(set) var channels = [Snowflake: GuildChannel]()
 
